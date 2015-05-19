0f1819f48ad3f6f553005627f2fc6b7561f03184
Chrjac <chrisjac@hiof.no>
2015-05-19 14:23:36 +0200

    Merge

--- Kode/Ruteplantjenesten/TravelRoute.cs
+++ Kode/Ruteplantjenesten/TravelRoute.cs
@@ -53,11 +53,15 @@ namespace Ruteplantjenesten
                 string final = stream.ReadToEnd();
 
                 var test = response.StatusCode;
-               
-                    final.Contains("directions");
+                if (final.Contains("directions"))
+                {
                     var b = CalculateResultFromJson(final);
                     return b;
-                
+                }
+                else
+                {
+                    throw new Exception();
+                }  
             }
             catch (Exception Ex)
             {
